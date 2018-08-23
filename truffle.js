719d8f4ba9ab6c78500a487d55bddddb75587fd7
daniellenarduzzi <daniellenarduzzi@gmail.com>
2018-08-23 11:07:43 -0300

    small fix
    changed host in kovan

--- truffle.js
+++ truffle.js
@@ -24,7 +24,7 @@ module.exports = {
       gas: 2900000
     },
       kovan: {
-      host: kovan.cryptologic.io:8787,
+      host: "kovan.cryptologic.io",
       port: 8747,
       network_id: "*",
       from: "0x44751576b07eee07de3d8d5bfb9c8dd77add1744"
